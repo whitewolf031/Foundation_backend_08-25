ed6dec5b73ba66e6c3c8c5a2098f5c81895ab53e
Sardorbek031 <w72075433@gmail.com>
2025-10-24 21:31:50 +0500

    finish the string methods

---
+++ string_method/string_methods.py
@@ -0,0 +1,88 @@
+# name = 'bobur'
+
+# # bosh harf uchun
+# m = name.capitalize()
+# print(m)
+
+# text ni o'rtaga joylashtirish
+# name = 'bobur'
+# print(name.center(10))
+# print('[' + 'Amirxon'.center(20, "*") + ']')
+
+# oxirgi elemtni tekshirish
+# # print(name.endswith("xon"))
+# if "epsilon".endswith("l"):
+#     print("yes")
+
+# else:
+#     print("No")
+
+# print("alpha".find("h")) #1
+# print("alpha".find("m")) #-1
+
+# print("python3".isalnum())
+# print("54564".isalnum())
+# print("python".isalnum())
+# print("python?".isalnum())
+
+# print("python3".isalpha())#False
+# print("54564".isalpha())#False
+# print("python".isalpha())#True
+# print("python?".isalpha())#False
+
+# print("python3".isdigit())#False
+# print("54564".isdigit())#True
+# print("python".isdigit())#False
+# print("python?".isdigit())#False
+
+# print("python3".islower())#True
+# print("привет".islower())#False
+# print("5pytHon".islower())#True
+# print("python?".islower())#True
+
+# print("python3   ".isspace())#True
+# print("привет".isspace())#False
+# print("  5pytHon".isspace())#True
+# print(" ".isspace())#True
+
+# print("PYTHoN".isupper())#True
+# print("привет".isupper())#False
+# print("5pytHon".isupper())#True
+# print("python?".isupper())#True
+
+# mevalar = ["Olma", "Anor", "Gilos"]
+# print(type(mevalar))
+# result = "[]".join(mevalar)
+# print(result)
+# print(type(result))
+
+# print("BOBUR".lower())
+# print("AMIrXON".lower())
+# print("MIRXON".lower())
+# print("ZIYODA".lower())
+
+# name = "Boburjon"
+# name2 = "Amirbek"
+# m = name2.replace("bek", "xon")
+# print(name.replace("jon", "xon"))
+# print(m)
+
+# mevalar = "Olma Anor Nok"
+# print(mevalar)
+# new_list = mevalar.split()
+# print(new_list)
+
+
+# oxirgi elemtni tekshirish
+# name = "Boburxon"
+# print(name.startswith("B"))
+# if "!5epsilon".startswith("!"):
+#     print("yes")
+
+# else:
+#     print("No")
+
+print("bobur".upper())
+print("amirxon".upper())
+print("mirxon".upper())
+print("ziyoda".upper())
